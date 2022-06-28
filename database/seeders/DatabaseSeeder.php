b4633707d316e45f4416bf21c7ea0828799bb5de
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2022-06-28 12:03:07 +0200

    add seedder for Posts table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,14 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
+        User    ::truncate();
         Category::truncate();
-
+        Post    ::truncate();
 
         $user = User::factory()->create();
-
-
-
 
         $personal = Category::create([
              'name' => 'Personal',
@@ -41,15 +38,32 @@ class DatabaseSeeder extends Seeder
              'slug' => 'work',
          ]);
 
-/*          Post::create([
-
+          Post::create([
+            'user_id'=> $user->id,
+            'category_id' => $family->id,
             'title' => 'My family post',
-            'slug' => 'mypfamily-post',
+            'slug' => 'my-family-post',
             'excerpt' => 'Lorem ipsum dollar sit amet.',
             'body' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
          ]);
- */
 
+         Post::create([
+            'user_id'=> $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My personal post',
+            'slug' => 'my-personal-post',
+            'excerpt' => 'Lorem ipsum dollar sit amet.',
+            'body' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+         ]);
+
+         Post::create([
+            'user_id'=> $user->id,
+            'category_id' => $work->id,
+            'title' => 'My work post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'Lorem ipsum dollar sit amet.',
+            'body' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+         ]);
 
 
 
